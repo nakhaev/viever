9af3799074ebec24a9d8498f3d5d91e825fc8cd3
Alex Nakhaev <a.nakhaev@gmail.com>
2022-01-06 16:52:00 +0200

    multilanguage for Footer component was added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import './Footer.scss';
-// import { Grid, List, ListItem, ListItemText } from '@mui/material';
+import {translate} from '../../services/translate.service';
 
 export default function Footer() {
     const currentYear = (new Date()).getFullYear();
     return(
         <Grid container  id="footer">
             <Grid item xs={12}>
-                <p>Copyright 2018-{currentYear} <a href="https://www.flaskdata.io/">Flaskdata.io</a></p>
+                <p>{ translate('FOOTER$COPYRIGHT') } 2018-{currentYear} <a href="https://www.flaskdata.io/">Flaskdata.io</a></p>
             </Grid>
             <Grid item xs={12}>
                 <ul className="list-inline footer-menu">
                     <li className="footer-link"><a
-                        href="https://app.flaskdata.io/privacy.html">Privacy Policy </a>
+                        href="https://app.flaskdata.io/privacy.html">Privacy Policy { translate('FOOTER$PRIVACY') }</a>
                     </li>
                     <li className="vertical-delimiter">&nbsp;</li>
                     <li className="footer-link"><a
-                        href="https://app.flaskdata.io/tos.html">Terms of Service </a>
+                        href="https://app.flaskdata.io/tos.html">Terms of Service { translate('FOOTER$TERMS_OF_USE') }</a>
                     </li>
                 </ul>
-                {/*<List dense={true}>*/}
-                {/*    <ListItem> <ListItemText primary="Single-line item" /> </ListItem>*/}
-                {/*    <ListItem> <ListItemText primary="Single-line item" /> </ListItem>*/}
-                {/*    <ListItem> <ListItemText primary="Single-line item" /> </ListItem>*/}
-                {/*</List>*/}
             </Grid>
         </Grid>
     )
