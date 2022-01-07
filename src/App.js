ea6391fc083c1e93e8dc6041c46490b85e8588b2
Alex Nakhaev <a.nakhaev@gmail.com>
2022-01-07 14:56:10 +0200

    unused code was deleted

--- src/App.js
+++ src/App.js
@@ -64,7 +64,6 @@ function App() {
 
     return (
         <div className={classes.join(' ')}>
-            <p>{direction}</p>
             <Switch>
                 {/*<Route path={'/'} exact component={IncorrectLink} />*/}
 
